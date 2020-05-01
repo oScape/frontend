924a2ae539a1fc38ae5fd83420f76d724b5be975
oScape <benoit.chassignol@gmail.com>
2020-05-01 17:54:43 +0200

    i am souled

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,10 +1,12 @@
 use lite_lib::component::Component;
 use lite_lib::components::{button::Button, select::Select};
 use lite_lib::listener::EventListener;
-use lite_lib::utils::document;
+use lite_lib::utils::{document, window};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{Event, HtmlElement, HtmlSelectElement};
+use web_sys::{Event, HtmlElement, HtmlSelectElement, Request, RequestInit, RequestMode, Response};
+use wasm_bindgen_futures::{spawn_local, JsFuture};
+use js_sys::Promise;
 
 #[wasm_bindgen]
 extern "C" {
@@ -15,38 +17,40 @@ extern "C" {
 }
 
 #[wasm_bindgen(start)]
-pub fn run() -> Result<(), JsValue> {
-    let button = Button::new("My first button", document().body().unwrap(), || {
-        log("An awsome mouse click")
-    });
-    button.render();
+pub async fn run() -> Result<(), JsValue> {
+    // let button = Button::new("My first button", document().body().unwrap(), || {
+    //     log("An awsome mouse click")
+    // });
+    // button.render();
 
-    EventListener::new(
-        document()
-            .get_element_by_id("button")
-            .unwrap()
-            .dyn_into::<HtmlElement>()
-            .unwrap(),
-        "click",
-        |_event| add_button(),
-    );
+    // EventListener::new(
+    //     document()
+    //         .get_element_by_id("button")
+    //         .unwrap()
+    //         .dyn_into::<HtmlElement>()
+    //         .unwrap(),
+    //     "click",
+    //     |_event| add_button(),
+    // );
 
-    let select = Select::new(
-        "My first select",
-        document().body().unwrap(),
-        vec!["First", "Second", "Third"],
-    );
-    select.render();
+    // let select = Select::new(
+    //     "My first select",
+    //     document().body().unwrap(),
+    //     vec!["First", "Second", "Third"],
+    // );
+    // select.render();
 
-    EventListener::new(
-        document()
-            .get_element_by_id("select")
-            .unwrap()
-            .dyn_into::<HtmlElement>()
-            .unwrap(),
-        "change",
-        |_event| catch_select(_event),
-    );
+    // EventListener::new(
+    //     document()
+    //         .get_element_by_id("select")
+    //         .unwrap()
+    //         .dyn_into::<HtmlElement>()
+    //         .unwrap(),
+    //     "change",
+    //     |_event| catch_select(_event),
+    // );
+
+    catch_select().await;
 
     Ok(())
 }
@@ -58,10 +62,30 @@ fn add_button() {
     button.render();
 }
 
-fn catch_select(_event: Event) {
-    let e_target = _event.current_target().unwrap();
-    let element = e_target.dyn_into::<HtmlSelectElement>().unwrap();
-    log(&element.value());
+async fn catch_select() {
+    let mut req = RequestInit::new();
+    req.method("GET");
+    req.mode(RequestMode::Cors);
+    let request =
+        Request::new_with_str_and_init("http://127.0.0.1:7878/data",&req).expect("Request could not be created");
+
+    let response = JsFuture::from(window().fetch_with_request(&request))
+        .await
+        .expect("Could not unwrap response");
+
+    // `response` is a `Response` object.
+    assert!(response.is_instance_of::<Response>());
+    let resp: Response = response.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let page = JsFuture::from(resp.text().unwrap()).await.unwrap().as_string().unwrap();
+    
+    log(&page);
+
+    // let e_target = _event.current_target().unwrap();
+    // let element = e_target.dyn_into::<HtmlSelectElement>().unwrap();
+
+    // log(&element.value());
 }
 
 #[wasm_bindgen]
