f91979a216ba70cef490cf02a1945f99ce960576
o0Ignition0o <jeremy.lempereur@gmail.com>
2020-05-03 17:30:46 +0200

    wasm-lib: tryouts with a callback function

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,12 +1,13 @@
+use js_sys::Promise;
 use lite_lib::component::Component;
 use lite_lib::components::{button::Button, select::Select};
 use lite_lib::listener::EventListener;
 use lite_lib::utils::{document, window};
+use std::task;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{Event, HtmlElement, HtmlSelectElement, Request, RequestInit, RequestMode, Response};
 use wasm_bindgen_futures::{spawn_local, JsFuture};
-use js_sys::Promise;
+use web_sys::{Event, HtmlElement, HtmlSelectElement, Request, RequestInit, RequestMode, Response};
 
 #[wasm_bindgen]
 extern "C" {
@@ -17,58 +18,54 @@ extern "C" {
 }
 
 #[wasm_bindgen(start)]
-pub async fn run() -> Result<(), JsValue> {
-    // let button = Button::new("My first button", document().body().unwrap(), || {
-    //     log("An awsome mouse click")
-    // });
-    // button.render();
+pub fn run() -> Result<(), JsValue> {
+    let button = Button::new("My first button", document().body().unwrap(), || {
+        log("An awsome mouse click")
+    });
+    button.render();
 
-    // EventListener::new(
-    //     document()
-    //         .get_element_by_id("button")
-    //         .unwrap()
-    //         .dyn_into::<HtmlElement>()
-    //         .unwrap(),
-    //     "click",
-    //     |_event| add_button(),
-    // );
+    EventListener::new(
+        document()
+            .get_element_by_id("button")
+            .unwrap()
+            .dyn_into::<HtmlElement>()
+            .unwrap(),
+        "click",
+        on_button_click,
+    );
 
-    // let select = Select::new(
-    //     "My first select",
-    //     document().body().unwrap(),
-    //     vec!["First", "Second", "Third"],
-    // );
-    // select.render();
+    let select = Select::new(
+        "My first select",
+        document().body().unwrap(),
+        vec!["First", "Second", "Third"],
+    );
+    select.render();
 
-    // EventListener::new(
-    //     document()
-    //         .get_element_by_id("select")
-    //         .unwrap()
-    //         .dyn_into::<HtmlElement>()
-    //         .unwrap(),
-    //     "change",
-    //     |_event| catch_select(_event),
-    // );
-
-    catch_select().await;
+    EventListener::new(
+        document()
+            .get_element_by_id("select")
+            .unwrap()
+            .dyn_into::<HtmlElement>()
+            .unwrap(),
+        "change",
+        on_select_change,
+    );
 
     Ok(())
 }
 
-fn add_button() {
-    let button = Button::new("My second button", document().body().unwrap(), || {
-        log("An second awsome mouse click")
-    });
-    button.render();
-}
-
-async fn catch_select() {
+fn on_select_change(_event: Event) {
     let mut req = RequestInit::new();
     req.method("GET");
     req.mode(RequestMode::Cors);
-    let request =
-        Request::new_with_str_and_init("http://127.0.0.1:7878/data",&req).expect("Request could not be created");
+    let request = Request::new_with_str_and_init("http://127.0.0.1:7878/data", &req)
+        .expect("Request could not be created");
+    //block until async shit is done
+    spawn_local(fetch_and_log_data(request));
+}
 
+async fn fetch_and_log_data(request: Request) {
+    // Fetch data from the server
     let response = JsFuture::from(window().fetch_with_request(&request))
         .await
         .expect("Could not unwrap response");
@@ -78,14 +75,21 @@ async fn catch_select() {
     let resp: Response = response.dyn_into().unwrap();
 
     // Convert this other `Promise` into a rust `Future`.
-    let page = JsFuture::from(resp.text().unwrap()).await.unwrap().as_string().unwrap();
-    
+    let page = JsFuture::from(resp.text().unwrap())
+        .await
+        .unwrap()
+        .as_string()
+        .unwrap();
+
+    // Log the fetched data
     log(&page);
+}
 
-    // let e_target = _event.current_target().unwrap();
-    // let element = e_target.dyn_into::<HtmlSelectElement>().unwrap();
-
-    // log(&element.value());
+fn on_button_click(_event: Event) {
+    let button = Button::new("My second button", document().body().unwrap(), || {
+        log("An second awsome mouse click")
+    });
+    button.render();
 }
 
 #[wasm_bindgen]
