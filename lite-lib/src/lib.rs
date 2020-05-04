1f94d55b6d4cc4c6a9613eced5b6dc1fb4024b39
oScape <benoit.chassignol@gmail.com>
2020-05-04 14:18:00 +0200

    create store singleton

--- lite-lib/src/lib.rs
+++ lite-lib/src/lib.rs
@@ -1,5 +1,9 @@
+#[macro_use]
+extern crate lazy_static;
+
 pub mod components;
 pub mod listener;
 pub mod utils;
+pub mod store;
 
 pub mod component;
---
+++ lite-lib/src/store/mod.rs
@@ -0,0 +1,18 @@
+struct Store {
+    data: Vec<String>
+}
+
+impl Store {
+    fn new() -> Store {
+        let data = vec!["yolo".to_owned()];
+        Store {
+            data
+        }
+    }
+}
+
+lazy_static! {
+    static ref STORE: Store = {
+        Store::new()
+    };
+}
