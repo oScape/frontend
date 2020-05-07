0e0d5557906343cad12bb885b949ae9269920a21
oScape <benoit.chassignol@gmail.com>
2020-05-07 22:15:42 +0200

    doc reducer

--- lite-lib/src/store/reducer.rs
+++ lite-lib/src/store/reducer.rs
@@ -1,7 +1,31 @@
-use std::collections::HashMap;
-
-#[derive(Debug)]
-pub struct Reducer {
-    name: String,
-    data: HashMap<String, String>,
-}
+/// A `Reducer` is a function which will apply an `Action` to a `State`
+/// called previousState and output a `State` called `nextState`.
+/// 
+/// We don't mutate the state, we create a new Struct.
+/// 
+/// # Example
+/// ```
+/// struct State {
+///    counter: i8
+/// }
+///
+/// enum Action {
+///     Increment,
+///     Decrement
+/// }
+///
+/// let reducer: Reducer<State, Action> = |state: State, action: Action| -> State {
+///     match action {
+///         Action::Increment => State {
+///             counter: state.counter + 1
+///         },
+///         Action::Decrement => State {
+///             counter: state.counter - 1
+///         },
+///         _ => State {
+///             counter: state
+///         },
+///     }
+/// };
+/// ```
+pub type Reducer<State, Action> = fn(State, Action) -> State;
