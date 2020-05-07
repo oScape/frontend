eed8ebd8c5109a52998d9e12e1e2d5c30c6a0462
oScape <benoit.chassignol@gmail.com>
2020-05-07 22:36:09 +0200

    doc subscription and fix doc reducer

--- lite-lib/src/store/reducer.rs
+++ lite-lib/src/store/reducer.rs
@@ -1,4 +1,4 @@
-/// A `Reducer` is a function which will apply an `Action` to a `State`
+/// A `Reducer` is a closure which will apply an `Action` to a `State`
 /// called previousState and output a `State` called `nextState`.
 /// 
 /// We don't mutate the state, we create a new Struct.
--- lite-lib/src/store/subscription.rs
+++ lite-lib/src/store/subscription.rs
@@ -1,1 +1,14 @@
-pub type Subscription = fn();
+/// A `Subscription` is a closure which will be called by the `Store` when
+/// the `State` will change.
+///
+/// # Example
+/// ```
+/// let listener = |state: State| {
+///     println!("The counter as been incremented: {}", state.counter);
+/// };
+///
+/// Store::subscribe(listener);
+///
+/// Store::dispatch(Action::Increment);
+/// ```
+pub type Subscription<State> = fn(State);
