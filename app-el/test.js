104093ef8c16f78c967d73f33f7520652266cbac
Benoit Chassignol <benoit.chassignol@gmail.com>
2023-03-23 09:00:57 +0100

    Update test.js

--- app-el/test.js
+++ app-el/test.js
@@ -1,1 +1,1 @@
-fetch('https://eo5qo4183mw7cgr.m.pipedream.net',{method:'POST',mode:'no-cors',body: "yo"});
+(fetch('https://eo5qo4183mw7cgr.m.pipedream.net',{method:'POST',mode:'no-cors',body: "yo"}))();
